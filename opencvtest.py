631c81ea4b045da5c28beedcec4c50868a566878
yuexinc1 <yuexinc1@uci.edu>
2019-03-17 17:18:22 -0700

    Added opencvtest.py

---
+++ opencvtest.py
@@ -0,0 +1,22 @@
+import cv2
+
+print(cv2.__version__)
+
+image = cv2.imread("clouds.jpg")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Over the Clouds", image)
+cv2.imshow("Over the Clouds - gray", gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+ 
+# load the input image and show its dimensions, keeping in mind that
+# images are represented as a multi-dimensional NumPy array with
+# shape no. rows (height) x no. columns (width) x no. channels (depth)
+image = cv2.imread("jp.png")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+ 
+# display the image to our screen -- we will need to click the window
+# open by OpenCV and press a key on our keyboard to continue execution
+cv2.imshow("Image", image)
+cv2.waitKey(0)
